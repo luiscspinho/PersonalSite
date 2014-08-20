f4f25eb791eafcec6e72039d5954952ed541fd39
Luis Pinho <Luis.Pinho@blip.pt>
2014-08-20 16:30:32 +0100

    Minor changes

--- src/app/app.js
+++ src/app/app.js
@@ -5,6 +5,7 @@ app.controller('socialController', ['$scope', function($scope){
     $scope.socialNetworks = [
                 {
                     name: "Twitter",
+                    id: "twitter",
                     link: "http://www.twitter.com/LuisCSPinho",
                     description: "Wanna know where am I flying?",
                     dataHover: "Or just mess around...",
@@ -14,6 +15,7 @@ app.controller('socialController', ['$scope', function($scope){
                 },
                 {
                     name: "LinkedIn",
+                    id: "linkedin",
                     link: "https://www.linkedin.com/in/luiscspinho",
                     description: "Why don't you link with me?",
                     dataHover: "On LinkedIn only...",
@@ -23,6 +25,7 @@ app.controller('socialController', ['$scope', function($scope){
                 },
                 {
                     name: "GitHub",
+                    id: "github",
                     link: "https://github.com/luiscspinho/",
                     description: "Some code of my own?",
                     dataHover: "Be careful...",
@@ -32,6 +35,7 @@ app.controller('socialController', ['$scope', function($scope){
                 },
                 {
                     name: "Lastfm",
+                    id: "lastfm",
                     link: "http://www.last.fm/user/LuisCSPinho",
                     description: "There is no better music for you to listen",
                     dataHover: "For real...",
@@ -41,6 +45,7 @@ app.controller('socialController', ['$scope', function($scope){
                 },
                 {
                     name: "I don't have Facebook!",
+                    id: "facebook",
                     link: "http://luispinhodoesnthavefacebook.com/",
                     description: "Simply my facebook page",
                     dataHover: "Facebook, facebook...",
@@ -50,6 +55,14 @@ app.controller('socialController', ['$scope', function($scope){
                     enable: true
                 }
                 ];
+
+    $scope.aboutMeInfo = "Hi. I am a QA Engineer with great passion for automation process, frontend technologies and web development. I am very passionate about Scrum and Agile methodologies.";
+
+    $scope.detailsAboutMe = {
+        name: "Luís Pinho",
+        age: "25",
+        email: "luiscspinho@gmail.com",
+    };
 
     $scope.events = {
         mouseEnter : function (social) {
@@ -65,8 +78,8 @@ app.directive("social", function () {
         //templateUrl: 'src/app/social/social.html',
         link: function ($scope, element, attrs) {
             element.bind('mouseenter', function() {
-                if($scope.social.animation && !$scope.enable){
-                    element.addClass('fixedFace');
+                if($scope.social.animation){
+                    angular.element(document.getElementById("facebook").getElementsByClassName("descriptionContainer")).addClass('fixedFace');
                     angular.element(document.getElementsByClassName('facebookIcon')).addClass('facebookDisabled');
                 }
             });
